580c1fac35590332fd53837f95c6288895629bfc
Jan Altensen <info@stricted.net>
2021-11-03 20:34:35 -0500

    gts3l-common: add RemovePackages
    just set LOCAL_SRC_FILES to /dev/null and it works again

--- gts3l.mk
+++ gts3l.mk
@@ -333,6 +333,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:root/init.recovery.qcom.rc
 
+# Remove Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
